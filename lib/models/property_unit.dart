7734b015f606a65f7a1664e546cb4defdf31c8d7
Ishatha-glitch <ikarani483@gmail.com>
2026-07-30 00:18:33 +0300

    Create PropertyUnit class with attributes and method
    Added PropertyUnit class with attributes and toMap method.

--- lib/models/property_unit.dart
+++ lib/models/property_unit.dart
@@ -1,1 +1,27 @@
+class PropertyUnit {
+  final String block;
+  final int floor;
+  final String unitNumber;
 
+  double rent;
+
+  bool occupied;
+
+  PropertyUnit({
+    required this.block,
+    required this.floor,
+    required this.unitNumber,
+    this.rent = 0,
+    this.occupied = false,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "block": block,
+      "floor": floor,
+      "unit_number": unitNumber,
+      "rent": rent,
+      "occupied": occupied,
+    };
+  }
+}
