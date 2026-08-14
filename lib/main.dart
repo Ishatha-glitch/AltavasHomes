5ed8d6b51788fb930e4db7c1cb542d8b5a7dc545
Ishatha-glitch <ikarani483@gmail.com>
2026-08-14 23:48:44 +0300

    Add imports for theme provider and provider package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
+import 'providers/theme_provider.dart';
 
 import 'providers/auth_provider.dart';
 import 'router.dart';
